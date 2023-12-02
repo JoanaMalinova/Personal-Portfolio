d8c5154b7b1357e294b7b35a2275fbe5999d281f
Yoana Malinova <110969425+JoanaMalinova@users.noreply.github.com>
2023-12-02 14:56:22 +0200

    finished portfolio, added firebase

---
+++ firebase_config.js
@@ -0,0 +1,12 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.4.0/firebase-app.js";;
+
+const firebaseConfig = {
+  apiKey: "AIzaSyD6wK6hG655VO-CA-Eqje38aDit1j2QnII",
+  authDomain: "portfolio-4946d.firebaseapp.com",
+  projectId: "portfolio-4946d",
+  storageBucket: "portfolio-4946d.appspot.com",
+  messagingSenderId: "851071600222",
+  appId: "1:851071600222:web:4f2934b1f7788e653956cf"
+};
+
+const app = initializeApp(firebaseConfig);
